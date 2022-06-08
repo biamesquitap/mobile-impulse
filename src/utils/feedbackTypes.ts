0815e5b17ed638c06a14dea9c362e318613aae16
biamesquitap <beatrizmesquitta@yahoo.com>
2022-06-08 15:10:36 -0300

    first

---
+++ src/utils/feedbackTypes.ts
@@ -0,0 +1,15 @@
+
+export const feedbackTypes ={
+  'BUG':{
+    title:'Problema',
+    image: required('../assets/bug.png')
+  },
+  'IDEA':{
+    title:'Ideia',
+    image: required('../assets/idea.png')
+  },
+'OTHER':{
+    title:'Outro',
+    image: required('../assets/other.png')
+  },
+};
